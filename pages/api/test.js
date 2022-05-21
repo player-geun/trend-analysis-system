8006e5b231038c4623cd4f8c8d554fad19f60891
dengoyoon <engus927@naver.com>
2022-05-22 00:48:43 +0900

    네이버 api 비동기적으로 불러오기 완료

--- pages/api/test.js
+++ pages/api/test.js
@@ -9,30 +9,26 @@ let hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
 hmac.update(timestamp + '.' + method + '.' + api_url);
 let hash = hmac.finalize();
 let sig = hash.toString(CryptoJS.enc.Base64); // 여기 까지 naver 검색광고 api 시그니쳐 키 만드는 코드 sig가 시그니처키가 됨!!
-let resultAPI = null;// 검색광고 api 결과담는 변수
-let result_api = null;// 네이버 트랜드 api 결과 담는 변수
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const keywords = req.query.keywords.split(',');
 
-  const adSearchData = getAdSearchData(keywords);
+  const adSearchData = await getAdSearchData(keywords);
 
-  console.log(adSearchData);
+  const trendAnalysisData = await getTrendAnalysisData(keywords);
 
-  getTrendAnalysisData(keywords);
+  console.log(trendAnalysisData);
 
-  return res.status(200).json({"dd" : "aa"});
+  return res.status(200).json(trendAnalysisData);
 
 }
 
-const getAdSearchData = async (hintKeywords) =>{
-  let url = 'https://api.naver.com/keywordstool?hintKeywords='+ encodeURI(hintKeywords) + '&showDetail=1'; // 검색광고 url
-  // const {
-  //   hintKeywords
-  // } = req.body; // 테스트 하려면 이 부분 주석 처리 하고 밑에 params: 부분에 검색하고 싶은 검색어를 hintkeywords 부분에 넣는다.
+const getAdSearchData = async (hintKeywords) => {
+  let url = 'https://api.naver.com/keywordstool?hintKeywords='+ encodeURI(hintKeywords) + '&showDetail=1';
+
   const result = await axios.get(url,{
     params:{
-      hintKeywords : hintKeywords, // parameter로 hintKeyword를 넘긴다 
+      hintKeywords : hintKeywords,
     },
     headers: {
       'X-Timestamp': timestamp,
@@ -41,60 +37,66 @@ const getAdSearchData = async (hintKeywords) =>{
       'X-CUSTOMER' : "2557032",
       'X-Signature': sig
     }
-  })
+  });
 
-  // console.log(result.data);
   return await result.data;
-  
 
-  // return result;
-  // .then(r => {
-  //   console.log("success!")
-  //   // resultAPI = r.data;
-  //   return await r.data;
-  //   // console.log(resultAPI);
-  //   // 클라이언트 쪽에 데이터를 보내는 코드를 주석 처리 하면 오류 해결!
-  //   // 만약 테스트를 하고 싶으면 console.log(resultAPI); 이부분 주석처리 하고 화면에 띄우는게 보기 더 편함
-  //   //return res.status(200).json(resultAPI)
-  //  })   
-  
-
-  // .catch((error) => { 
-  //   // Failure
-  //   console.log(error)
-  // })
-  // return await result;
 }
 
-const getTrendAnalysisData = async() => {
+const getTrendAnalysisData = async(keywords) => {
+  const keywordGroups = [];
+
+  const [start, end] = getLastMonthDate();
+
+  keywords.forEach(keyword => {
+    keywordGroups.push({
+      groupName: keyword,
+      keywords: [keyword] 
+    })
+  });
+
   try {
     const request_body = {
-      startDate: "2017-10-01",
-      endDate: "2020-10-30",
-      timeUnit: "month",
-      keywordGroups: [
-        { groupName: "치킨", keywords: ["BBQ", "BHC", "교촌치킨"] },
-        { groupName: "떡볶이", keywords: ["엽기떡볶이","신전떡볶이", "배떡"] },
-      ],
+      startDate: start,
+      endDate: end,
+      timeUnit: "date",
+      keywordGroups: keywordGroups,
     };
-      const url = "https://openapi.naver.com/v1/datalab/search";
-      const headers={ 
-        'Content-type': 'application/json', 
-        'Accept': 'application/json',
-        'X-Naver-Client-Id' :  "PKjTmaEjasvkR4a2qbEy",
-        'X-Naver-Client-Secret' : "zQC7scyLbz",
-        'Access-Control-Allow-Origin' : '*',
-        'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
-      } 
-      const api_result = await axios.post(url, request_body, {
-          headers: headers,
-      });
-      
-      result_api=api_result.data
-      // console.log(result_api);
-      //여기는 화면에 보여줌
-      // return res.status(200).json(result_api);
-      }catch(error){
-       console.log(error);
-      }
+    const url = "https://openapi.naver.com/v1/datalab/search";
+    const headers={ 
+      'Content-type': 'application/json', 
+      'Accept': 'application/json',
+      'X-Naver-Client-Id' :  "PKjTmaEjasvkR4a2qbEy",
+      'X-Naver-Client-Secret' : "zQC7scyLbz",
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+    } 
+    const result = await axios.post(url, request_body, {
+        headers: headers,
+    });
+    
+    return result.data;
+
+    } catch(error){
+      console.log(error);
+    }
+}
+
+const getLastMonthDate = () => {
+  const today = new Date();
+
+  const year = today.getFullYear();
+  const month = ('0' + (today.getMonth() + 1)).slice(-2);
+  const day = ('0' + today.getDate()).slice(-2);
+
+  const todayString = year + '-' + month  + '-' + day;
+
+  const oneMonthAgo = new Date(today.setMonth(today.getMonth() - 1));
+  const agoYear = oneMonthAgo.getFullYear();
+  const agoMonth = ('0' + (oneMonthAgo.getMonth() + 1)).slice(-2);
+  const agoDay = ('0' + oneMonthAgo.getDate()).slice(-2);
+
+  const oneMonthAgoString = agoYear + '-' + agoMonth  + '-' + agoDay;
+
+  return [oneMonthAgoString, todayString];
 }
