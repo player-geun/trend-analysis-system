d88fa467cd979064a34ec32c5be6615c9cd0f8ac
dengoyoon <engus927@naver.com>
2022-05-25 03:56:58 +0900

    절댓값 계산 오류 수정

--- pages/api/chart/category.js
+++ pages/api/chart/category.js
@@ -24,8 +24,8 @@ export default async function handler(req, res) {
     });
   }
 
-  const [absoluteValuePerOneRatio, changedKeywords] = await getAbsolutesPerOneRatio(keywords);
-  const absoluteValuesEachDate = await getAbsoluteValuesEachDate(startDate, endDate, changedKeywords, absoluteValuePerOneRatio);
+  const [absoluteValuePerOneRatio, trendAnalysisData, changedKeywords] = await getAbsolutesPerOneRatio(startDate, endDate, keywords);
+  const absoluteValuesEachDate = await getAbsoluteValuesEachDate(startDate, endDate, changedKeywords, trendAnalysisData, absoluteValuePerOneRatio);
 
   const result = {
     startDate : startDate,
@@ -43,12 +43,14 @@ export default async function handler(req, res) {
 }
 
 // 1ratio당 검색량을 구하는 부분
-const getAbsolutesPerOneRatio = async(keywords) => {
+const getAbsolutesPerOneRatio = async(startDate, endDate, keywords) => {
   let result;
+  // 검색광고
   const [recentMonthAbsoluteValue, changedKeywords] = await getRecentMonthAllAbsoluteValue(keywords);
-  const recentMonthRatio = await getRecentMonthAllRatio(changedKeywords);
+  // 트렌드 분석
+  const [recentMonthRatio, trendAnalysisData] = await getRecentMonthAllRatio(startDate, changedKeywords);
   result = recentMonthAbsoluteValue / recentMonthRatio;
-  return [result, changedKeywords];
+  return [result, trendAnalysisData, changedKeywords];
 }
 
 const getRecentMonthAllAbsoluteValue = async(keywords) => {
@@ -62,32 +64,32 @@ const getRecentMonthAllAbsoluteValue = async(keywords) => {
   return [result, keywords];
 }
 
-const getRecentMonthAllRatio = async(keywords) => {
+const getRecentMonthAllRatio = async(startDate, keywords) => {
   let result = 0;
-  const [start, end] = getLastMonthDate();
-  const recentMonthTrendAnalysisData = await getTrendAnalysisData(start, end, keywords);
-  recentMonthTrendAnalysisData.results.forEach(keywordInfo => {
+  // 트렌드 분석 API 호출은 조회 시작 날짜로 부터 오늘까지의 데이터를 호출.
+  const trendAnalysisData = await getTrendAnalysisData(startDate, getTodayDate(), keywords);
+  trendAnalysisData.results.forEach(keywordInfo => {
     let allRatio = 0;
-    keywordInfo.data.forEach(data => {
+    const startToTodayLength = keywordInfo.data.length;
+    keywordInfo.data.slice(startToTodayLength - 31, startToTodayLength).forEach(data => {
       allRatio += data.ratio;
     })
     result += allRatio;
   })
   
-  return result;
+  return [result, trendAnalysisData];
 }
 
 // 조회기간에서 각 날짜당 검색량을 구하는 부분
-const getAbsoluteValuesEachDate = async(startDate, endDate, keywords, absoluteValuePerOneRatio) => {
-  const trendAnalysisData = await getTrendAnalysisData(startDate, endDate, keywords);
-
+const getAbsoluteValuesEachDate = async(startDate, endDate, keywords, trendAnalysisData, absoluteValuePerOneRatio) => {
   const result = [];
   
   keywords.forEach((keyword, index) => {
     const keywordAmountArray = [];
-    trendAnalysisData.results[index].data.forEach(data => {
+    trendAnalysisData.results[index].data.slice(0, getDiffBetweenTwoDates(startDate, endDate)).forEach(data => {
       keywordAmountArray.push({
         period : data.period,
+        ratio : data.ratio,
         amount : data.ratio * absoluteValuePerOneRatio
       })
     })
@@ -130,8 +132,6 @@ const getAdSearchData = async(hintKeywords) => {
 
 }
 
-
-
 const getTrendAnalysisData = async(startDate, endDate, keywords) => {
   const keywordGroups = [];
 
@@ -169,26 +169,29 @@ const getTrendAnalysisData = async(startDate, endDate, keywords) => {
     }
 }
 
-const getLastMonthDate = () => {
-  const today = new Date();
-
-  const year = today.getFullYear();
-  const month = ('0' + (today.getMonth() + 1)).slice(-2);
-  const day = ('0' + today.getDate()).slice(-2);
-
-  const todayString = year + '-' + month  + '-' + day;
-
-  const oneMonthAgo = new Date(today.setMonth(today.getMonth() - 1));
-  const agoYear = oneMonthAgo.getFullYear();
-  const agoMonth = ('0' + (oneMonthAgo.getMonth() + 1)).slice(-2);
-  const agoDay = ('0' + oneMonthAgo.getDate()).slice(-2);
-
-  const oneMonthAgoString = agoYear + '-' + agoMonth  + '-' + agoDay;
-
-  return [oneMonthAgoString, todayString];
+const getTodayDate = () => {
+    const today = new Date();
+  
+    const year = today.getFullYear();
+    const month = ('0' + (today.getMonth() + 1)).slice(-2);
+    const day = ('0' + today.getDate()).slice(-2);
+  
+    const todayString = year + '-' + month  + '-' + day;
+  
+    return todayString;
 }
 
 const findCategory = async(categoryName) => {
     const data = await keywordModel.find({categoryName : categoryName});
     return data;
+}
+
+const getDiffBetweenTwoDates = (_date1, _date2) => {
+    const date1 = new Date(_date1);
+    const date2 = new Date(_date2);
+
+    const diffDate = date1.getTime() - date2.getTime();
+    const dateDays = Math.abs(diffDate / (1000 * 3600 * 24));
+    
+    return dateDays + 1;
 }
