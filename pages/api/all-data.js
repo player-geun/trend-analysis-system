57a9db18a944681e1c686ee995ee2c606d4bbca6
kdc4867 <62419262+kdc4867@users.noreply.github.com>
2022-05-30 12:49:00 +0900

    db결과값에 따라 수정 예정

--- pages/api/all-data.js
+++ pages/api/all-data.js
@@ -2,28 +2,28 @@
 //- GET api/all-data
 import dbInit from './dbInit.js';
 import keywordModel from './models/Keyword.js';
-import keywordSchema from './models/Keyword.js';
 
 dbInit();
 
 export default function handler(req, res) {
-   const apiMethod = req.method;
+   
+   const allKeywords = keywordModel();
 
-   keywordSchema.find(function(){
-     if(apiMethod != "GET"){
+   keywordModel.find(function(){
+     if(req.method != "GET"){
        return res.status(200).json({
         isSuccess : false,
         code : 2000,
         message : "Wrong method.",
      });
     }else{
-    const allKeywords = keywordSchema;
+    
 
        return res.status(200).json({
         isScucess : true,
         code : 1000,
         message : "Successed.",
-        result : allKeywords,
+        result : ({ allKeywords }),
       });
     }
   });
