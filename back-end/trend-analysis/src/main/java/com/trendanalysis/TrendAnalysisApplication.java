7c4c91359c6dc810f5290b60855146f688e0b449
player-geun <player_geun@naver.com>
2022-11-01 22:25:50 +0900

    init - 스프링 서버 초기화
    1. 사용 기술
     -Java 11
     -Spring, Spring MVC, Spring Boot(2.7.5)
     -Spring Data MongoDB
     -Gradle

---
+++ back-end/trend-analysis/src/main/java/com/trendanalysis/TrendAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.trendanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrendAnalysisApplication.class, args);
+    }
+
+}
