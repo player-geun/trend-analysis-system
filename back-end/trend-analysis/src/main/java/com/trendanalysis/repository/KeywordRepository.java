dbdfd6aa94cdb0e34e3753b01677756e4e77647d
player-geun <player_geun@naver.com>
2022-11-02 16:58:59 +0900

    Repository #1 - 리포지토리 함수 추가
    1. id 값을 클라이언트와 통신환경에서 사용하는 것이 보안상에도 좋지 않다고 판단하여 Name 기반의 리포지토리 함수 생성
    2. keyword는 개략적으로만 개발

--- back-end/trend-analysis/src/main/java/com/trendanalysis/repository/KeywordRepository.java
+++ back-end/trend-analysis/src/main/java/com/trendanalysis/repository/KeywordRepository.java
@@ -1,10 +1,10 @@
 package com.trendanalysis.repository;
 
-import com.trendanalysis.entity.KeywordEntity;
+import com.trendanalysis.entity.Keyword;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface KeywordRepository extends MongoRepository<KeywordEntity, ObjectId> {
+public interface KeywordRepository extends MongoRepository<Keyword, ObjectId> {
 }
