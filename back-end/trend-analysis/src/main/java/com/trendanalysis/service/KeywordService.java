e12e31609cfc9d6037f8d87f07adbb439b526e60
player-geun <player_geun@naver.com>
2022-11-06 13:20:23 +0900

    Service #2 - 수정 및 추가
    1. name 기반의 로직 대신 id 기반의 로직으로 변경
    2. update와 save 시에 dirty checking이 되지 않는 문제 해결
    -> 1.1 이슈 해결
    3. key에 대한 방어로직 형성
    -> exception handler를 따로 만들어서 처리할 예정

--- back-end/trend-analysis/src/main/java/com/trendanalysis/service/KeywordService.java
+++ back-end/trend-analysis/src/main/java/com/trendanalysis/service/KeywordService.java
@@ -1,7 +1,47 @@
 package com.trendanalysis.service;
 
+import com.trendanalysis.domain.Category;
+import com.trendanalysis.domain.Keyword;
+import com.trendanalysis.repository.CategoryRepository;
+import com.trendanalysis.repository.KeywordRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class KeywordService {
+
+    private final KeywordRepository keywordRepository;
+    private final CategoryRepository categoryRepository;
+
+    public void save(Keyword keyword, List<String> categoryNames) {
+        if (keywordRepository.findByName(keyword.getName()) != null) {
+            throw new IllegalStateException("이미 존재하는 키워드입니다.");
+        }
+        List<Category> list = new ArrayList<>();
+
+        for (String name : categoryNames) {
+            Category category = categoryRepository.findByName(name);
+            list.add(category);
+        }
+
+        Keyword newKeyword = Keyword.builder().name(keyword.getName()).categories(list).build();
+
+        keywordRepository.save(newKeyword);
+
+        for (String name : categoryNames) {
+            Category category = categoryRepository.findByName(name);
+            Keyword updatedKey = keywordRepository.findByName(keyword.getName());
+            updatedKey.update(Keyword.builder()
+                    .name(category.getName())
+                    .categories(new ArrayList<Category>())
+                    .build());
+
+            category.getKeywords().add(updatedKey);
+            categoryRepository.save(category);
+        }
+    }
 }
