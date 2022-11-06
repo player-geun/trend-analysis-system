1b7613be6ffd2048df77b327b2508b446b6cff8d
player-geun <player_geun@naver.com>
2022-11-06 13:16:48 +0900

    Repository #2 - 추가
    이름으로 조회하는 함수 추가

--- back-end/trend-analysis/src/main/java/com/trendanalysis/repository/CategoryRepository.java
+++ back-end/trend-analysis/src/main/java/com/trendanalysis/repository/CategoryRepository.java
@@ -1,13 +1,17 @@
 package com.trendanalysis.repository;
 
-import com.trendanalysis.entity.Category;
+import com.trendanalysis.domain.Category;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoryRepository extends MongoRepository<Category, ObjectId> {
     Category findByName(String Name);
 
     void deleteByName(String Name);
+
+     List<Category> findAllByParentId(String id);
 }
