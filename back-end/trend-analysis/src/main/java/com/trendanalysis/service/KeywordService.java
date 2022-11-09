d9f3d3575208dd6e9b3eb09eb5cff3c9077c674c
player-geun <player_geun@naver.com>
2022-11-10 00:45:41 +0900

    Service #2 - 매핑 관계 고려
    1. 매핑 관계를 고려하여 하나의 document를 삭제했을 때 해당 파일을 참조하고 있는 다른 document도 같이 갱신
    2. 검색량 API 절댓값 로직 작성

--- back-end/trend-analysis/src/main/java/com/trendanalysis/service/KeywordService.java
+++ back-end/trend-analysis/src/main/java/com/trendanalysis/service/KeywordService.java
@@ -2,13 +2,16 @@ package com.trendanalysis.service;
 
 import com.trendanalysis.domain.Category;
 import com.trendanalysis.domain.Keyword;
+import com.trendanalysis.dto.KeywordResponseDto;
 import com.trendanalysis.repository.CategoryRepository;
 import com.trendanalysis.repository.KeywordRepository;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,15 +20,31 @@ public class KeywordService {
     private final KeywordRepository keywordRepository;
     private final CategoryRepository categoryRepository;
 
+    public KeywordResponseDto findOne(ObjectId keywordId) {
+        Keyword keyword = keywordRepository.findById(keywordId).get();
+        List<ObjectId> categoryIds = keyword.getCategories();
+
+        List<String> categoryNames = categoryIds.stream().map(id ->
+                categoryRepository.findById(id).get().getName()).collect(Collectors.toList());
+
+
+        return new KeywordResponseDto(
+                keyword.getId().toString(),
+                keyword.getName(),
+                categoryNames,
+                keyword.getCreatedAt()
+        );
+    }
+
     public Keyword save(Keyword keyword, List<String> categoryNames) {
         if (keywordRepository.findByName(keyword.getName()) != null) {
             throw new IllegalStateException("이미 존재하는 키워드입니다.");
         }
-        List<Category> list = new ArrayList<>();
+        List<ObjectId> list = new ArrayList<>();
 
         for (String name : categoryNames) {
             Category category = categoryRepository.findByName(name);
-            list.add(category);
+            list.add(category.getId());
         }
 
         Keyword newKeyword = Keyword.builder().name(keyword.getName()).categories(list).build();
@@ -36,14 +55,73 @@ public class KeywordService {
             Category category = categoryRepository.findByName(name);
             Keyword updatedKey = keywordRepository.findByName(keyword.getName());
             updatedKey.update(Keyword.builder()
-                    .name(category.getName())
-                    .categories(new ArrayList<Category>())
+                    .name(updatedKey.getName())
+                    .categories(new ArrayList<ObjectId>())
                     .build());
 
-            category.getKeywords().add(updatedKey);
+            category.getKeywords().add(updatedKey.getId());
             categoryRepository.save(category);
         }
 
         return save;
+    }
+
+    public Keyword update(String updatedKeywordName, List<String> categoryNames, ObjectId id) {
+        Keyword originKeyword = keywordRepository.findById(id).get();
+        List<ObjectId> notUpdatedCategory = new ArrayList<>();
+
+        for (ObjectId categoryId : originKeyword.getCategories()) {
+            if (!categoryNames.contains(categoryRepository.findById(categoryId).get().getName())) {
+                //해당 카테고리 안에 키워드 삭제
+                Category category = categoryRepository.findById(categoryId).get();
+                category.getKeywords().remove(id);
+                categoryRepository.save(category);
+                notUpdatedCategory.add(categoryId);
+            }
+        }
+
+        //수정됬거나 삭제된 카테고리 -> 모두 삭제
+        for (ObjectId categoryId : notUpdatedCategory) {
+            originKeyword.getCategories().remove(categoryId);
+        }
+
+        //변경되지 않은 카테고리 추가
+        for (String categoryName : categoryNames) {
+            Category temp = categoryRepository.findByName(categoryName);
+
+            if (!originKeyword.getCategories()
+                    .contains(categoryRepository.findByName(categoryName).getId())) {
+                Category category = categoryRepository.findByName(categoryName);
+                originKeyword.getCategories().add(category.getId());
+
+                //새로운 카테고리에 키워드 추가
+                temp.getKeywords().add(id);
+                categoryRepository.save(temp);
+            }
+        }
+
+        Keyword updatedKeyword = Keyword.builder()
+                .name(updatedKeywordName)
+                .categories(originKeyword.getCategories())
+                .build();
+
+
+        originKeyword.update(updatedKeyword);
+        keywordRepository.save(originKeyword);
+
+        return originKeyword;
+    }
+
+    public void delete(ObjectId id) {
+        List<Category> all = categoryRepository.findAll();
+
+        for (Category category : all) {
+            if (category.getKeywords().contains(id)) {
+                category.getKeywords().remove(id);
+                categoryRepository.save(category);
+            }
+        }
+
+        keywordRepository.deleteById(id);
     }
 }
